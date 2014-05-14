b7690de961a328cf6b365a85728a72438c4f8d83
jonathangean <jonathangean@gmail.com>
2014-05-14 16:45:06 -0400

    working styled

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,6 @@
 class Song < ActiveRecord::Base
+  has_and_belongs_to_many :listeners
+
   def self.itunes_search(query)
     escaped_query = query.downcase.gsub(' ', '+')
     query_string = "entity=musicTrack&limit=20&term=#{escaped_query}"
@@ -8,12 +10,29 @@ class Song < ActiveRecord::Base
     raw_song = response['results']
     package_songs = raw_song.map do |song|
       {
-        :itunes_artist_id => song['artistID'],
-        :itunes_song_id => song['trackID'],
+        :itunes_artist_id => song['artistId'],
+        :itunes_song_id => song['trackId'],
         :title => song['trackName'],
         :artwork_url => song['artworkUrl100']
       }
     end
     package_songs
+  end
+
+  def self.itunes_lookup(id)
+    url = "http://itunes.apple.com/lookup?id=#{id}"
+    raw_response = HTTParty.get(url)
+    response = JSON.parse(raw_response)
+    raw_song = response['results'].first
+    song_hash =  {
+      title: raw_song['trackName'],
+      album: raw_song['collectionName'],
+      genre: raw_song['primaryGenreName'],
+      preview_link: raw_song['previewUrl'],
+      artwork_url: raw_song['artworkUrl100']
+      }
+
+      Song.new(song_hash)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'songs/search' => 'songs#search'
-
+  post 'songs/save' => 'songs#save'
+  delete 'songs/:id' => 'songs#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
